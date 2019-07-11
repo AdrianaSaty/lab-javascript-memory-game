400a21e0eab5f47c9c2b2e878b88868c5b13c305
Adriana Saty Bertelli <adri-saty@hotmail.com>
2019-07-10 21:26:55 -0300

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -23,29 +23,64 @@ var cards = [
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
   { name: 'thor',            img: 'thor.jpg' }
-];
-var memoryGame = new MemoryGame(cards);
-
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var html = '';
+ ];
+ var memoryGame = new MemoryGame(cards);
+ 
+ document.addEventListener("DOMContentLoaded", function(event) {
+  memoryGame.shuffleCards();
+  let html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+ 
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
-
+ 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
+  document.querySelectorAll('.card').forEach(function(card, index) {
     card.onclick = function() {
-      // TODO: write some code here
+      memoryGame.pickedCards.push(card)
+      card.children[0].classList.toggle('back');
+      card.children[0].classList.toggle('front');
+      card.children[1].classList.toggle('front');
+      card.children[1].classList.toggle('back');
+ 
+      if (memoryGame.pickedCards.length === 2) {
+        let card1 = memoryGame.pickedCards[0].getAttribute('data-card-name');
+        let card2 = memoryGame.pickedCards[1].getAttribute('data-card-name');
+        let guessed = memoryGame.checkIfPair(card1, card2)
+        console.log(guessed)
+        document.getElementById('pairs_clicked').innerHTML = memoryGame.pairsClicked
+        document.getElementById('pairs_guessed').innerHTML = memoryGame.pairsGuessed;
+ 
+        if (guessed) {
+          memoryGame.pickedCards[0].children[1].classList.add('blocked');
+          memoryGame.pickedCards[1].children[1].classList.add('blocked');
+          memoryGame.pickedCards = [];
+ 
+          let gameOver = memoryGame.isFinished()
+          if (gameOver) {
+            alert('Parabéns! Você terminou o jogo.')
+          }
+ 
+        } else {
+          setTimeout(() => {
+            memoryGame.pickedCards[0].children[0].classList.toggle('back');
+            memoryGame.pickedCards[0].children[0].classList.toggle('front');
+            memoryGame.pickedCards[0].children[1].classList.toggle('back');
+            memoryGame.pickedCards[0].children[1].classList.toggle('front');
+            memoryGame.pickedCards[1].children[0].classList.toggle('front');
+            memoryGame.pickedCards[1].children[0].classList.toggle('back');
+            memoryGame.pickedCards[1].children[1].classList.toggle('front');
+            memoryGame.pickedCards[1].children[1].classList.toggle('back');
+            memoryGame.pickedCards = [];
+          }, 1000);
+        }
+      }
       console.log('Card clicked')
     }
   });
-});
-
-
+ });
